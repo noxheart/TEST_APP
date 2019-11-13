7f1ed033c339f7c85330b86ae4d149b21e4481b4
Noxheart <daniel-97@live.se>
2019-11-13 15:25:41 +0100

    first commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TEST_APP.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string[] Food 
+        {
+            get;
+            set;
+        }
+        public string getUrl
+        {
+            get;
+            private set;
+        }
+        public void OnGet()
+        {
+            Food = new string[]
+            {
+                "Tacos", "Pizza", "Pasta", "Sallad", "Sushi"
+            };
+            getUrl = Request.Query["produkt"];
+        }
+        
+    }
+}
